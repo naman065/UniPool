6198285b1134bf0a3592731fb8d5b59d3cd838c0
Naman <singhnaman065@gmail.com>
2026-03-25 18:49:54 +0530

    fixed repo structure

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:unipool/main.dart';
-
-void main() {
-  test('app widget can be constructed', () {
-    expect(const UnipoolApp(), isNotNull);
-  });
-}
